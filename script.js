c0479fcd375825a4df1db7821272c480e11939a5
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2020-10-31 09:48:23 -0500

    modified the charList and made them into individual variables for upper, lower, numbers, and symbols. I also added confirms for the same vars.

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
+function generatePassword (){
+  var question = prompt("what is the desired password length?")
+  console.log('question:', question)
+  var lowCaps = confirm("lowerCase?")
+  console.log('lowCaps:', lowCaps)
+  var Caps = confirm("upperCase?")
+  console.log('Caps:', Caps)
+  var numbers = confirm("Numbers?")
+  console.log('numbers:', numbers)
+  var symbols = confirm("Symbols?")
+  console.log('symbols:', symbols)
+ }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// User clicks generate button
+
+// Display set of questions to the user 
+//     - Prompt for password length
+//     - Ask four types of chars  - confirms
+
+// Generate password
+//     - Need all the char types
+//     - Determine what chars to pick from
+//     - Loop thru how many times for the password length and pick a random char
+//         - Build a password
+//     - Return password
+
+//     - 
+
